6bb0baddb6d0c2435e5534d58643b75a6da732fd
Marcos German M <marcosgermanm@outlook.com>
2022-01-30 09:04:19 -0300

    (1)+ funcion para tachar tareas realizadas al clickear item de lista.     (2)+ funcion para eliminar tareas al dar doble click a item de lista.     (3)modificacion de estilo de la caja de texto y su contenedor.

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ let toDoContainer=document.getElementById('ToDoContainer');
 
 AddButtonToDo.addEventListener('click',function(){
     var paragraph=document.createElement('p');
+    paragraph.classList.add("Stylo");
     paragraph.innerText=inputField.value;
     toDoContainer.appendChild(paragraph);
+    inputField.value="";
 
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration="line-through";
+
+    });
+
+    paragraph.addEventListener('dblclick',function(){
+        paragraph.parentNode.removeChild(paragraph);
+    })
 });
